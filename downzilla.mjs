947685db852addabc81271b2eaf679bb2f88deea
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-20 17:38:51 +0100

    Update downzilla.mjs

--- downzilla.mjs
+++ downzilla.mjs
@@ -15,7 +15,8 @@ import feedbackdb from "./db/feedbackdb.mjs"
 dotenv.config()
 const origin = [
     "http://127.0.0.1:5500",
-    "http://localhost:5500"
+    "http://localhost:5500",
+    "https://downzilla.netlify.app"
 ]
 
 app.use(cors({
