1937d6b3c5444fa01088206142c6ca0f18aa83fb
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-20 16:42:39 +0100

    Update feedback.mjs

--- extra/Feedback/feedback.mjs
+++ extra/Feedback/feedback.mjs
@@ -11,7 +11,7 @@ const givefeedback = async(req, res) => {
             })
         }
         
-        const check = feedbackS.findOne({$or [{userId: id}]})
+        const check = feedbackS.findOne({$or: [{userId: id}]})
         
         if (ckeck) {
             await feedbackS.findByIdAndDelete(check._id)
