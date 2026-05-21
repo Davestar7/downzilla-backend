b2773b924a823ffc926b28d00e51acf6aac08f66
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-21 12:42:52 +0100

    Update mongoDB.mjs

--- db/mongoDB.mjs
+++ db/mongoDB.mjs
@@ -2,10 +2,9 @@ import mongoose from "mongoose";
 
 const connectionToDownzilla = async () => {
     try {
-       const connect = mongoose.createConnection(process.env.MONGOOSE_URI, {
+       const connect = await mongoose.createConnection(process.env.MONGOOSE_URI, {
             writeConcern: { w: "majority" }
-        })
-        await connect.asPromise()
+        }).asPromise()
         return connect;
     } catch (error) {
         console.error("failed to get DataBase check internet connection: ", error.message);
