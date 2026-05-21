f268872a6ba8d6ac5010e30085bb48e343f8c7a2
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-21 11:22:10 +0100

    Update mongoDB.mjs

--- db/mongoDB.mjs
+++ db/mongoDB.mjs
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 const connectionToDB = async () => {
     try {
-        await mongoose.createConnection(process.env.MONGOOSE_URI, {
+       const connect = await mongoose.createConnection(process.env.MONGOOSE_URI, {
             writeConcern: { w: "majority" }
         })
         console.log("connected to database successfully!!!")
+        return connect;
     } catch (error) {
         console.error("failed to get DataBase check internet connection: ", error.message);
         process.exit(1)
