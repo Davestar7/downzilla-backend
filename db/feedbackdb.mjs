a23c2203b8d48e6f34b4f2b69a1893b47d6e6d77
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-21 12:29:51 +0100

    Update feedbackdb.mjs

--- db/feedbackdb.mjs
+++ db/feedbackdb.mjs
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToFeedback = async() => {
     try {
-        const connect = await mongoose.createConnection(process.env.FEEDBACKDBURL, {
+        const connect = mongoose.createConnection(process.env.FEEDBACKDBURL, {
             writeConcern: { w: "majority" }
         });
         await connect.asPromise()
