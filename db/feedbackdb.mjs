9963bfd9b29e5e0e83575dada5889dae18ce8573
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-21 11:22:53 +0100

    Update feedbackdb.mjs

--- db/feedbackdb.mjs
+++ db/feedbackdb.mjs
@@ -2,11 +2,12 @@ import mongoose from "mongoose";
 
 async function connectToFeedback() {
     try {
-        await mongoose.createConnection(process.env.FEEDBACKDBURL, {
+        const connect = await mongoose.createConnection(process.env.FEEDBACKDBURL, {
             writeConcern: { w: "majority" }
         });
         
         console.log("feed back database connected")
+        return connect;
     } catch (e) {
         console.log(e.message)
         process.exit(1)
