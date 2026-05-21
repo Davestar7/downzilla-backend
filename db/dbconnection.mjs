2c387da882bae139538e23ca8f00cf29fd885394
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-21 12:11:38 +0100

    Create dbconnection.mjs

---
+++ db/dbconnection.mjs
@@ -0,0 +1,15 @@
+import Feedback from "./feedbackdb.mjs"
+import connectioToDownzilla from "./mongodb.mjs"
+
+const connectionToDB = async () => {
+     try {
+        await connectionToDownzilla.asPromise()
+        await Feedback.asPromise()
+
+        console.log("connected to database successfully✌️✌️✌️")
+     } catch (e) {
+        console.log("😔connection to database failed " + e.message)
+        process.exit(1)
+     }
+
+export default connectionToDB
