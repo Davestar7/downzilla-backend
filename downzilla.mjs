8bf2764146dff28aa69d0b1e3b9627c421807cfb
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-24 17:48:12 +0100

    Update downzilla.mjs

--- downzilla.mjs
+++ downzilla.mjs
@@ -16,7 +16,9 @@ dotenv.config()
 const origin = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://downzilla.netlify.app"
+    "https://downzilla.netlify.app",
+    "www.downzilla.buzz",
+    "downzilla.buzz"
 ]
 
 app.use(cors({
