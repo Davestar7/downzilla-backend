b30c2ecb24689fea9123a977080752096ab52b61
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-24 09:59:35 +0100

    Update loginSignin.mjs

--- middleware/loginSignin.mjs
+++ middleware/loginSignin.mjs
@@ -19,7 +19,7 @@ const usersignIn = async (req, res) => {
         if (!ifemail.ok) {
             return res.status(400).json({
                 success: false,
-                message: `email ${ifemail.details}`
+                message: `email error: ${ifemail.details}`
             })
         }
 
