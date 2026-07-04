1d67e2651dc642548a19e3ff8674c66c7541f266
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-07-04 18:04:26 +0100

    Fix static file serving path in downzilla.mjs

--- downzilla.mjs
+++ downzilla.mjs
@@ -29,7 +29,7 @@ app.use(cors({
     credentials: true
 }))
 app.use(express.json())
-app.use(express.static(...))
+app.use(express.static("."))
 // app.use(express.json({ limit: "6mb" }))
 app.use(cookieParser())
 app.use(bodyParser.json())
